d361ac213c463a45e367f9a339fe03aa08ebc123
huszerldani <huszerldani@gmail.com>
2021-10-06 17:47:59 +0200

    Debug kód törölve

--- src/SzunetNapokServiceProvider.php
+++ src/SzunetNapokServiceProvider.php
@@ -6,7 +6,7 @@ use Illuminate\Support\ServiceProvider;
 
 class SzunetNapokServiceProvider extends ServiceProvider {
 	public function boot() {
-		//dd("ennyi");
+
 	}
 
 	public function register() {
